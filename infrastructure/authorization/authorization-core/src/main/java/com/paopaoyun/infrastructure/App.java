fe9573459e1d37387b0b0e6386791c02f077dbd6
sidney <sidney@exttech.com>
2013-11-14 23:52:22 +0800

    init commit

---
+++ infrastructure/authorization/authorization-core/src/main/java/com/paopaoyun/infrastructure/App.java
@@ -0,0 +1,13 @@
+package com.paopaoyun.infrastructure;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
